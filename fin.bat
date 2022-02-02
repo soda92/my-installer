124d7e6e8e63741aafa2a12ddbefacee8281b7be
SodaCris <18463922396@163.com>
2022-02-02 11:22:28 +0800

    add lfs

--- fin.bat
+++ fin.bat
@@ -1,3 +1,3 @@
 cd /D %~dp0
 py wait-finish.py
-pyw qt_simple_window.py
+py qt_simple_window.py
--- wait-finish.py
+++ wait-finish.py
@@ -1,21 +1,23 @@
 import subprocess
+import os
 
-subprocess.run(
-    [
-        "C:/Install/postgresql-14.1-1-windows-x64.exe",
-        "--mode",
-        "unattended",
-        "--unattendedmodeui",
-        "minimalWithDialogs",
-        "--prefix",
-        "C:\PostgreSQL_14",
-        "--datadir",
-        "C:\PostgreSQL_14\data",
-        "--create_shortcuts",
-        "0",
-        "--superpassword",
-        "1234",
-        "--locale",
-        "en",
-    ]
-)
+if not os.path.exists("C:/PostgreSQL_14/bin/psql.exe"):
+    subprocess.run(
+        [
+            "C:/Install/postgresql-14.1-1-windows-x64.exe",
+            "--mode",
+            "unattended",
+            "--unattendedmodeui",
+            "minimalWithDialogs",
+            "--prefix",
+            "C:\PostgreSQL_14",
+            "--datadir",
+            "C:\PostgreSQL_14\data",
+            "--create_shortcuts",
+            "0",
+            "--superpassword",
+            "1234",
+            "--locale",
+            "en",
+        ]
+    )
